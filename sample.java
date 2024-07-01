6ae39a25a4c77c1a69032782ea94050372a12e1f
Sobitha2804 <23203031@srcas.ac.in>
2024-07-01 11:58:05 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,11 @@
+class sample{
+  public static void main(String[]Args)
+  {
+    System.out.println("Hello");
+    System.out.println("program");
+    System.out.println("java");
+    system.out.println("devops");
+  }
+}
+    
+    
